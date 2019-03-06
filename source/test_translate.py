e0b92c8e062248800a8b5bd93809baf8003a66af
Ogaday <w.ogaday@gmail.com>
2019-03-06 13:41:04 +0000

    Feat: Add bulk translation function

--- source/test_translate.py
+++ source/test_translate.py
@@ -2,7 +2,7 @@ import unittest
 
 import vcr
 
-from source.translate import translate
+from source.translate import translate, bulk_translate
 
 
 class TestTranslate(unittest.TestCase):
@@ -13,3 +13,23 @@ class TestTranslate(unittest.TestCase):
     def test_translate_from_en_to_gb(self):
         translated_text = translate("Hello, world!", from_="en", to="de")
         self.assertEqual(translated_text, "Hallo Welt!")
+
+    @vcr.use_cassette(
+            "cassettes/test_bulk_translate_from_en_to_de.yml",
+            filter_headers=["Ocp-Apim-Subscription-Key"]
+        )
+    def test_bulk_translate_from_en_to_gb(self):
+        source_texts = [
+                "Hello, world",
+                "This is an exercise in coding",
+                "I like writing tests"
+            ]
+        expected_texts = [
+                "Hallo Welt",
+                "Dies ist eine Übung in der Codierung",
+                "Ich schreibe gerne Tests"
+            ]
+        translated_texts = bulk_translate(source_texts, from_="en", to="de")
+        for translated, expected in zip(translated_texts, expected_texts):
+            with self.subTest():
+                self.assertEqual(translated, expected)
--- source/translate.py
+++ source/translate.py
@@ -55,3 +55,35 @@ def translate(source_text, to, from_=None):
         raise
 
     return r.json()[0]["translations"][0]["text"]
+
+
+def bulk_translate(source_texts, to, from_=None):
+    session = requests.Session()
+    retry = Retry(connect=3, backoff_factor=0.5)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+
+    headers = {
+            "Ocp-Apim-Subscription-Key": KEY,
+            "Content-type": "application/json",
+            "X-ClientTraceId": str(uuid.uuid4())
+        }
+
+    params = {
+            "api-version": "3.0",
+            "to": to
+        }
+    if from_ is not None:
+        params["from"] = from_
+
+    body = [{"text": source_text} for source_text in source_texts]
+
+    r = session.post(ENDPOINT, headers=headers, params=params, json=body)
+    try:
+        r.raise_for_status()
+    except requests.HTTPError:
+        # Error handling
+        raise
+
+    return [t['translations'][0]['text'] for t in r.json()]
